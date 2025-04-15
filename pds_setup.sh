7ac0b6a947c8678d86ebe99863322d81e78a041f
Rick-Fusion <r.c.v.schaik@outlook.com>
2025-04-15 11:26:53 +0200

    Updated pds_setup.sh file

--- pds_setup.sh
+++ pds_setup.sh
@@ -30,11 +30,15 @@ module purge
 module load libxml2
 module load SuiteSparse/7.7.0-intel-2023b
 module load IMAS/4.0.0-2024.12-intel-2023b
-module load IMASPy
 module load MUSCLE3
 module load IDS-Validator
 module load MATLAB
 module load GCC
+
+module unload IMAS-AL-Python
+module load IMAS-Python
+
+
 
 # SET UP METIS
 git clone ssh://git@git.iter.org/scen/metis.git
@@ -46,15 +50,14 @@ deactivate
 cd ..
 
 # SET UP NICE
-# dd v4 needs fix
 git clone git@gitlab.inria.fr:blfauger/nice.git
 cd nice
 git submodule init
 git submodule update
-cp run/iwrap/param/inv/param.x* run/input
+cp run/iwrap/param/inv/iter/param.x* run/input
 cp run/iwrap/param/xsd/param.x* run/input
 cd src
-cp Makefile.DO-NOT-CHANGE-THIS-FILE Makefile
+cp Makefile.TEMPLATE Makefile
 make -j nice_imas_inv_muscle3
 make -j nice_imas_dir_muscle3
 make -j nice_imas_evo_muscle3
@@ -78,22 +81,56 @@ pip install -e .
 deactivate
 cd ..
  
-# SET UP TORAX
+
+# SET UP TORAX (from TORAX README at 15/04/2025)
 module purge
 module load Python/3.11.5-GCCcore-13.2.0
+sudo apt-get install python3-tk
+
+# set up torax-m3 and virtual environment
 git clone ssh://git@git.iter.org/scen/torax-m3.git
 cd torax-m3
 python3 -m venv ./venv
 . venv/bin/activate
 cd ..
-git clone git@github.com:google-deepmind/torax.git
+
+
+# Create a code directory where you will install the TORAX dependencies. (virtual environment in torax-m3)
+mkdir torax_dir
+cd torax_dir
+
+# Install QLKNN_7_11
+git clone https://github.com/google-deepmind/fusion_surrogates.git
+pip install -e ./fusion_surrogates
+export TORAX_QLKNN_MODEL_PATH="$PWD"/fusion_surrogates/fusion_surrogates/models/qlknn_7_11.qlknn
+echo export TORAX_QLKNN_MODEL_PATH="$PWD"/fusion_surrogates/fusion_surrogates/models/qlknn_7_11.qlknn >> ~/.bashrc
+
+
+# long term version when IMAS coupling is implemented in main TORAX branch
+#git clone git@github.com:google-deepmind/torax.git
+#cd torax
+#pip install -e .
+#cd ..
+#cd torax-m3
+#pip install -e .
+#deactivate
+#cd ..
+
+
+# For now use Mike's git fork of TORAX (with IMAS coupling)
+git clone git@github.com:mikesndrs/torax.git
 cd torax
 pip install -e .
-cd ..
+cd ../..
 cd torax-m3
 pip install -e .
 deactivate
 cd ..
+
+
+
+
+
 
 # END MESSAGE
 echo 'You can try out the test couplings in the pds/ymmsl_files directory by running:'
