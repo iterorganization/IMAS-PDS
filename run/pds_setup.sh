66826b728ea6f3b69769a5ec85612a77a94481de
Sanders Mike EXT <sanderm@sdcc-login03.iter.org>
2025-05-01 11:42:54 +0200

    fix torax install by manually installing muscle3

--- run/pds_setup.sh
+++ run/pds_setup.sh
@@ -25,95 +25,121 @@
 pip install --upgrade pip
 pip install --upgrade setuptools wheel
 
+INSTALL_PDS=true
+INSTALL_IMAS_MUSCLE3=true
+INSTALL_METIS=true
+INSTALL_NICE=true
+INSTALL_TORAX=true
+
 # SET UP PDS
-echo "############## INSTALLING PDS ##############"
-source imas_base_env
-git clone ssh://git@git.iter.org/scen/pds.git
-cd pds
-git checkout feature/sdcc_install_instructions
-python3 -m venv ./venv
-. venv/bin/activate
-pip install -e .
-deactivate
-module purge
-cd ..
-bash setup_test_files.bash
-echo "############## FINISHED PDS ##############"
+if [ "$INSTALL_PDS" == true ]; then
+  echo "############## INSTALLING PDS ##############"
+  source imas_base_env
+  git clone ssh://git@git.iter.org/scen/pds.git
+  cd pds
+  git checkout feature/sdcc_install_instructions
+  python3 -m venv ./venv
+  . venv/bin/activate
+  pip install -e .
+  deactivate
+  module purge
+  cd ..
+  bash setup_test_files.bash
+  echo "############## FINISHED PDS ##############"
+fi
 
 # SET UP IMAS-M3
-echo "############## INSTALLING IMAS-MUSCLE3 ##############"
-source imas_base_env
-module load IDS-Validator
-git clone git@github.com:iterorganization/IMAS-muscle3.git
-cd IMAS-muscle3
-git checkout readme
-python3 -m venv ./venv
-. venv/bin/activate
-pip install -e .
-deactivate
-module purge
-cd ..
-echo "############## FINISHED IMAS-MUSCLE3 ##############"
+if [ "$INSTALL_IMAS_MUSCLE3" == true ]; then
+  echo "############## INSTALLING IMAS-MUSCLE3 ##############"
+  source imas_base_env
+  module load IDS-Validator
+  git clone git@github.com:iterorganization/IMAS-muscle3.git
+  cd IMAS-muscle3
+  git checkout readme
+  python3 -m venv ./venv
+  . venv/bin/activate
+  pip install -e .
+  deactivate
+  module purge
+  cd ..
+  echo "############## FINISHED IMAS-MUSCLE3 ##############"
+fi
 
-# # SET UP METIS
-# echo "############## INSTALLING METIS ##############"
-# source imas_base_env
-# module load MATLAB
-# module load GCC
-# git clone ssh://git@git.iter.org/scen/metis.git
-# cd metis
-# python3 -m venv ./venv
-# . venv/bin/activate
-# matlab -nodisplay -nosplash -r "make_metis_linux; exit"
-# deactivate
-# cd ..
-# module purge
-# echo "############## FINISHED METIS ##############"
+# SET UP METIS
+if [ "$INSTALL_METIS" == true ]; then
+  echo "############## INSTALLING METIS ##############"
+  source imas_base_env
+  module load MATLAB
+  module load GCC
+  git clone ssh://git@git.iter.org/scen/metis.git
+  cd metis
+  git checkout muscle3
+  python3 -m venv ./venv
+  # . venv/bin/activate
+  # matlab -nodisplay -nosplash -r "make_metis_linux; exit"
+  # deactivate
+  cd ..
+  module purge
+  echo "############## FINISHED METIS ##############"
+fi
 
 # SET UP NICE
-echo "############## INSTALLING NICE ##############"
-source imas_base_env
-module load SuiteSparse/7.7.0-intel-2023b
-module load libxml2
-git clone git@gitlab.inria.fr:blfauger/nice.git
-cd nice
-git submodule init
-git submodule update
-git checkout develop            # For now change to develop branch, otherwise next command won't work
-cp run/iwrap/param/inv/iter/param.x* run/input
-cp run/iwrap/param/xsd/param.x* run/input
-cd src
-cp Makefile.TEMPLATE Makefile
-make -j nice_imas_inv_muscle3
-make -j nice_imas_dir_muscle3
-make -j nice_imas_evo_muscle3
-module purge
-cd ../..
-echo "############## FINISHED NICE ##############"
+if [ "$INSTALL_NICE" == true ]; then
+  echo "############## INSTALLING NICE ##############"
+  source imas_base_env
+  module load SuiteSparse/7.7.0-intel-2023b
+  module load libxml2
+  git clone git@gitlab.inria.fr:blfauger/nice.git
+  cd nice
+  git submodule init
+  git submodule update
+  git checkout develop            # For now change to develop branch, otherwise next command won't work
+  cp run/iwrap/param/inv/iter/param.x* run/input
+  cp run/iwrap/param/xsd/param.x* run/input
+  cd src
+  cp Makefile.TEMPLATE Makefile
+  make -j nice_imas_inv_muscle3
+  make -j nice_imas_dir_muscle3
+  make -j nice_imas_evo_muscle3
+  module purge
+  cd ../..
+  echo "############## FINISHED NICE ##############"
+fi
 
 # SET UP TORAX-M3
-echo "############## INSTALLING TORAX-M3 ##############"
-# pip install 'imas-python @ git+ssh://git@github.com/iterorganization/imas-python.git@develop'
-# pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@be3f8c4b1'
-# git checkout 6c0ed3915
-source torax_base_env
-git clone ssh://git@git.iter.org/scen/torax-m3.git
-cd torax-m3
-git checkout feature/muscle3_actor
-python -m venv ./venv
-. venv/bin/activate
-pip install --upgrade pip
-pip install build
-pip install 'numpy > 2'
-# pip install imas-python
-pip install 'imas-python @ git+ssh://git@github.com/mikesndrs/imas-python.git@feature/enable-numpy-2.0'
-pip install 'imas_core @ git+ssh://git@git.iter.org/imas/al-core.git@develop'
-pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@feature/IMAS_coupling'
-pip install -e .
-deactivate
-module purge
-cd ..
-echo "############## FINISHED TORAX-M3 ##############"
+if [ "$INSTALL_TORAX" == true ]; then
+  echo "############## INSTALLING TORAX-M3 ##############"
+  # pip install 'imas-python @ git+ssh://git@github.com/iterorganization/imas-python.git@develop'
+  # pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@be3f8c4b1d93ca2bb22149a6d3da9ce04b7a0c78'
+  # git checkout 4dfa77c4688684cda646f55d8b4db992a3107d4f
+  source torax_base_env
+  git clone ssh://git@git.iter.org/scen/torax-m3.git
+  cd torax-m3
+  # git checkout feature/muscle3_actor
+  git checkout 4dfa77c4688684cda646f55d8b4db992a3107d4f
+  python -m venv ./venv
+  . venv/bin/activate
+  pip install --upgrade pip
+  pip install build
+  echo "############## INSTALLING NUMPY ##############"
+  pip install 'numpy > 2'
+  echo "############## INSTALLING IMAS-PYTHON ##############"
+  # pip install imas-python
+  pip install 'imas-python @ git+ssh://git@github.com/mikesndrs/imas-python.git@feature/enable-numpy-2.0'
+  echo "############## INSTALLING IMAS-CORE ##############"
+  pip install 'imas_core @ git+ssh://git@git.iter.org/imas/al-core.git@develop'
+  echo "############## INSTALLING TORAX ##############"
+  # pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@feature/IMAS_coupling'
+  pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@be3f8c4b1d93ca2bb22149a6d3da9ce04b7a0c78'
+  echo "############## INSTALLING MUSCLE3 ##############"
+  pip install muscle3
+  echo "############## INSTALLING TORAX-M3 ##############"
+  pip install -e . --no-deps
+  deactivate
+  module purge
+  cd ..
+  echo "############## FINISHED TORAX-M3 ##############"
+fi
 
 # END MESSAGE
 echo 'You can try out the test couplings in the pds/ymmsl_files directory by running:'
