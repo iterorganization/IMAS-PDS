a85befaf03fab51e8096ff64529c0b027c8cc014
Sanders Mike EXT <sanderm@sdcc-login02.iter.org>
2025-06-23 15:10:37 +0200

    add list of branches and conditional installation to setup bash script

--- run/pds_setup.sh
+++ run/pds_setup.sh
@@ -18,30 +18,31 @@
 # https://gitlab.inria.fr/blfauger/nice/-/wikis/home
 # https://torax.readthedocs.io/en/latest/installation.html
 
-# works for:
-#  WFE      - a9ec29c6df8850ba1982772c25fb70b99f5ed68a
-#  NICE     - b308cc394eeaea099e1926cdd5aae2067469cef2
-#  torax_m3 - c5633a9466283c6fe56b14063003a21654e35f75
-#  torax    - 9bc36562370448020409507eca2ac1f7ddd1bddf
+INSTALL_PDS="true"
+INSTALL_IMAS_MUSCLE3="true"
+INSTALL_WAVEFORM_EDITOR="true"
+INSTALL_METIS="true"
+INSTALL_NICE="true"
+INSTALL_TORAX="true"
+
+BRANCH_PDS='feature/sdcc_install_instructions'
+BRANCH_IMAS_MUSCLE3='main'
+BRANCH_WAVEFORM_EDITOR='main'
+BRANCH_METIS='muscle3'
+BRANCH_NICE='master'
+BRANCH_TORAX='feature/IMAS_coupling'
+BRANCH_TORAX_M3='feature/muscle3_actor'
 
 # MODULE LOAD
 pip install --upgrade pip
 pip install --upgrade setuptools wheel
 
-INSTALL_PDS=true
-INSTALL_IMAS_MUSCLE3=true
-INSTALL_WAVEFORM_EDITOR=true
-INSTALL_METIS=true
-INSTALL_NICE=true
-INSTALL_TORAX=true
-
 # SET UP PDS
-if [ "$INSTALL_PDS" == true ]; then
+if [ "$INSTALL_PDS" = "true" ] && [ ! -d "pds" ]; then
   echo "############## INSTALLING PDS ##############"
   source imas_base_env
-  git clone ssh://git@git.iter.org/scen/pds.git
+  git clone ssh://git@git.iter.org/scen/pds.git -b $BRANCH_PDS
   cd pds
-  git checkout feature/sdcc_install_instructions
   python3 -m venv ./venv
   . venv/bin/activate
   pip install -e .
@@ -53,13 +54,12 @@ if [ "$INSTALL_PDS" == true ]; then
 fi
 
 # SET UP IMAS-M3
-if [ "$INSTALL_IMAS_MUSCLE3" == true ]; then
+if [ "$INSTALL_IMAS_MUSCLE3" = "true" ] && [ ! -d "IMAS-muscle3" ]; then
   echo "############## INSTALLING IMAS-MUSCLE3 ##############"
   source imas_base_env
   module load IDS-Validator
-  git clone git@github.com:iterorganization/IMAS-muscle3.git
+  git clone git@github.com:iterorganization/IMAS-muscle3.git -b $BRANCH_IMAS_MUSCLE3
   cd IMAS-muscle3
-  git checkout readme
   python3 -m venv ./venv
   . venv/bin/activate
   pip install -e .
@@ -70,13 +70,11 @@ if [ "$INSTALL_IMAS_MUSCLE3" == true ]; then
 fi
 
 # SET UP WAVEFORM-EDITOR
-if [ "$INSTALL_WAVEFORM_EDITOR" == true ]; then
+if [ "$INSTALL_WAVEFORM_EDITOR" = "true" ] && [ ! -d "Waveform-Editor" ]; then
   echo "############## INSTALLING WAVEFORM-EDITOR ##############"
   source imas_base_env
-  git clone git@github.com:iterorganization/Waveform-Editor.git
+  git clone git@github.com:iterorganization/Waveform-Editor.git -b $BRANCH_WAVEFORM_EDITOR
   cd Waveform-Editor
-  # git checkout develop
-  git checkout feature/muscle3-actor
   python3 -m venv ./venv
   . venv/bin/activate
   pip install -e .[muscle3]
@@ -87,9 +85,9 @@ if [ "$INSTALL_WAVEFORM_EDITOR" == true ]; then
 fi
 
 # SET UP METIS
-if [ "$INSTALL_METIS" == true ]; then
+if [ "$INSTALL_METIS" = "true" ] && [ ! -d "metis" ]; then
   echo "############## INSTALLING METIS ##############"
-  git clone ssh://git@git.iter.org/scen/metis.git -b muscle3
+  git clone ssh://git@git.iter.org/scen/metis.git -b $BRANCH_METIS
   cd metis
   matlab -nodisplay -batch zineb_path
   cd ..
@@ -97,16 +95,15 @@ if [ "$INSTALL_METIS" == true ]; then
 fi
 
 # SET UP NICE
-if [ "$INSTALL_NICE" == true ]; then
+if [ "$INSTALL_NICE" = "true" ] && [ ! -d "nice" ]; then
   echo "############## INSTALLING NICE ##############"
   source imas_base_env
   module load SuiteSparse/7.7.0-intel-2023b
   module load libxml2
-  git clone git@gitlab.inria.fr:blfauger/nice.git
+  git clone git@gitlab.inria.fr:blfauger/nice.git -b $BRANCH_NICE
   cd nice
   git submodule init
   git submodule update
-  git checkout develop            # For now change to develop branch, otherwise next command won't work
   cp run/iwrap/param/inv/iter/param.x* run/input
   cp run/iwrap/param/xsd/param.x* run/input
   cd src
@@ -120,13 +117,11 @@ if [ "$INSTALL_NICE" == true ]; then
 fi
 
 # SET UP TORAX-M3
-if [ "$INSTALL_TORAX" == true ]; then
+if [ "$INSTALL_TORAX" = "true" ] && [ ! -d "torax-m3" ]; then
   echo "############## INSTALLING TORAX-M3 ##############"
   source torax_base_env
-  git clone ssh://git@git.iter.org/scen/torax-m3.git
+  git clone ssh://git@git.iter.org/scen/torax-m3.git -b $BRANCH_TORAX_M3
   cd torax-m3
-  # git checkout feature/muscle3_actor
-  git checkout c5633a9466283c6fe56b14063003a21654e35f75
   python -m venv ./venv
   . venv/bin/activate
   pip install --upgrade pip
@@ -134,8 +129,7 @@ if [ "$INSTALL_TORAX" == true ]; then
   pip install 'numpy > 2'
   pip install 'imas-python @ git+ssh://git@github.com/mikesndrs/imas-python.git@feature/enable-numpy-2.0'
   pip install 'imas_core @ git+ssh://git@git.iter.org/imas/al-core.git@develop'
-  # pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@feature/IMAS_coupling'
-  pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@9bc36562370448020409507eca2ac1f7ddd1bddf'
+  pip install 'torax @ git+ssh://git@github.com/mikesndrs/torax.git@feature/IMAS_coupling'
   pip install muscle3
   pip install -e . --no-deps
   deactivate
