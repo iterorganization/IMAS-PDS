cc5cf6ebbd25866b9932281ff1fe4becf85b0741
Mike Sanders <msanders@ignitioncomputing.com>
2025-09-03 10:50:47 +0200

    rough version of plotting script

--- scripts/plot_validation.py
+++ scripts/plot_validation.py
@@ -8,36 +8,47 @@ import numpy as np
 from imas import DBEntry, IDSFactory
 from imas.ids_defs import CLOSEST_INTERP
 
+# SHOT_NR = 105084
+# T_LIST = [10, 150, 270]
 SHOT_NR = 105092
-ORIGINAL_PATH = f"/home/ITER/sanderm/gitrepos/pds/run/temp_data/{SHOT_NR}_in"
-RESULT_PATH = f"/home/ITER/sanderm/gitrepos/pds/run/temp_data/{SHOT_NR}_out"
+T_LIST = [10, 110, 150]
+
+PATHS = {
+  # 'dina': f"/home/ITER/sanderm/gitrepos/pds/scenario_configs/{SHOT_NR}/tmp/data/{SHOT_NR}_in",
+  'nice': f"/home/ITER/sanderm/gitrepos/pds/scenario_configs/{SHOT_NR}/tmp/data/{SHOT_NR}_out_nice",
+  'torax': f"/home/ITER/sanderm/gitrepos/pds/scenario_configs/{SHOT_NR}/tmp/data/{SHOT_NR}_out_torax",
+}
 EQUILIBRIUM_FIGURE_PATH = f"/home/ITER/sanderm/gitrepos/pds/run/tmp/pds_run_equilibrium_{SHOT_NR}.png"
 COIL_FIGURE_PATH = f"/home/ITER/sanderm/gitrepos/pds/run/tmp/pds_run_coils_{SHOT_NR}.png"
 
 EQUILIBRIUM_FIELDS_0D = [
-  ('global_quantities', 'ip'),
-  ('global_quantities', 'magnetic_axis', 'r'),
-  ('global_quantities', 'magnetic_axis', 'z'),
-  ('global_quantities', 'magnetic_axis', 'b_field_phi'),
-  ('global_quantities', 'li_3'),
-  ('global_quantities', 'beta_pol'),
-  ('global_quantities', 'beta_tor'),
-  ('boundary', 'psi'),
-  ('boundary', 'elongation'),
-  ('boundary', 'triangularity'),
-  # ('x-point'),
+  # ('global_quantities', 'ip'),
+  # ('global_quantities', 'magnetic_axis', 'b_field_phi'),
+  # ('global_quantities', 'magnetic_axis', 'r'),
+  # ('global_quantities', 'magnetic_axis', 'z'),
+  # ('global_quantities', 'beta_pol'),
+  # ('global_quantities', 'beta_tor'),
+  # ('global_quantities', 'li_3'),
+  # ('boundary', 'elongation'),
+  # ('boundary', 'psi'),
+  # ('boundary', 'triangularity'),
 ]
 
 EQUILIBRIUM_FIELDS_1D = [
-  ('profiles_1d', 'q'),
-  ('profiles_1d', 'psi'),
   ('profiles_1d', 'pressure'),
   ('profiles_1d', 'dpressure_dpsi'),
+  ('profiles_1d', 'f'),
   ('profiles_1d', 'f_df_dpsi'),
+  ('profiles_1d', 'psi'),
+  ('profiles_1d', 'q'),
+  ('profiles_1d', 'j_phi'),
+  ('profiles_1d', 'gm2'),
+  # ('profiles_1d', 'volume'),
   # ('profiles_1d', 'elongation'),
-  # ('profiles_1d', 'triangularity_lower'),
-  # ('x-point'),
 ]
+
+PLOT_KWARGS = {'marker': '.'}
+GET_KWARGS = {'interpolation_method': CLOSEST_INTERP, 'lazy': True}
 
 def nested_getattr(obj, name_list):
   if len(name_list) == 0:
@@ -47,93 +58,96 @@ def nested_getattr(obj, name_list):
     return nested_getattr(new_obj, name_list[1:])
 
 def main():
-  db_org = DBEntry(f"imas:hdf5?path={ORIGINAL_PATH}", 'r')
-  db_res = DBEntry(f"imas:hdf5?path={RESULT_PATH}", 'r')
+  dbs = {
+    key: DBEntry(f"imas:hdf5?path={path}", 'r')
+    for key, path in PATHS.items()
+  }
 
-  ###############
-  # EQUILIBRIUM #
-  ###############
+  equilibrium_plots(dbs)
+  # pf_active_plots(dbs)
+
+  for db in dbs.values():
+    db.close()
+
+def equilibrium_plots(dbs):
   eq_dict = {}
-  t_list = [10, 150, 270]
-  equilibrium = db_org.get('equilibrium', lazy=True)
-  get_kwargs = {'interpolation_method': CLOSEST_INTERP, 'lazy': True}
+  equilibrium = list(dbs.values())[0].get('equilibrium', lazy=True)
   for t in equilibrium.time:
-    eq_org = db_org.get_slice('equilibrium', time_requested=t, **get_kwargs)
-    eq_res = db_res.get_slice('equilibrium', time_requested=t, **get_kwargs)
+    eqs = {
+      key: db.get_slice('equilibrium', time_requested=t, **GET_KWARGS)
+      for key, db in dbs.items()
+    }
     for field in EQUILIBRIUM_FIELDS_0D:
       if field[-1] not in eq_dict:
-        eq_dict[field[-1]] = {'org': [], 'res': []}
-      val_org = nested_getattr(eq_org.time_slice[0], field).value
-      val_res = nested_getattr(eq_res.time_slice[0], field).value
+        eq_dict[field[-1]] = {key: [] for key in dbs.keys()}
+      vals = {
+        key: nested_getattr(eqs[key].time_slice[0], field).value
+        for key in dbs.keys()
+      }
 
-      if 'boundary' in field and hasattr(eq_res.time_slice[0].profiles_1d, field[-1]):
-        res_arr = getattr(eq_res.time_slice[0].profiles_1d, field[-1]).value
-        res_psi = eq_res.time_slice[0].profiles_1d.psi.value
-        res_psi_norm = abs(res_psi - res_psi[0]) / abs(res_psi[-1] - res_psi[0])
-        val_res = np.interp(0.99, res_psi_norm, res_arr)
+      for key in dbs.keys():
+        if key == 'dina':
+          continue 
+        if 'boundary' in field and hasattr(eqs[key].time_slice[0].profiles_1d, field[-1]):
+          nice_arr = getattr(eqs[key].time_slice[0].profiles_1d, field[-1]).value
+          nice_psi = eqs[key].time_slice[0].profiles_1d.psi.value
+          nice_psi_norm = abs(nice_psi - nice_psi[0]) / abs(nice_psi[-1] - nice_psi[0])
+          vals[key] = np.interp(0.99, nice_psi_norm, nice_arr)
 
-      eq_dict[field[-1]]['org'].append(val_org)
-      eq_dict[field[-1]]['res'].append(val_res)
-  nrows, ncols = (4, 4)
-  fig, axes = plt.subplots(nrows=nrows, ncols=nrows, figsize=(12,12))
+      for key in dbs.keys():
+        eq_dict[field[-1]][key].append(vals[key])
+  nrows, ncols = (4, 2)
+  fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12,12))
   axes = axes.flatten()
   colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-  plot_kwargs = {'marker': '.'}
-  for i, (key, val) in enumerate(eq_dict.items()): 
-    axes[i].set_title(key)
-    axes[i].set_ylabel(key)
+  for i, (field_key, val) in enumerate(eq_dict.items()): 
+    axes[i].set_title(field_key)
+    axes[i].set_ylabel(field_key)
     axes[i].set_xlabel('time')
-    axes[i].plot(equilibrium.time, eq_dict[key]['org'], label='org', **plot_kwargs)
-    axes[i].plot(equilibrium.time, eq_dict[key]['res'], label='res', **plot_kwargs)
+    for key in dbs.keys():
+      axes[i].plot(equilibrium.time, eq_dict[field_key][key], label=key, **PLOT_KWARGS)
     axes[i].legend()
   for i, field in enumerate(EQUILIBRIUM_FIELDS_1D):
     idx = len(eq_dict) + i
-    for i_t, t in enumerate(t_list):
-      color = colors[i_t]
-      eq_org = db_org.get_slice('equilibrium', time_requested=t, **get_kwargs)
-      eq_res = db_res.get_slice('equilibrium', time_requested=t, **get_kwargs)
-      val_org = nested_getattr(eq_org.time_slice[0], field)
-      val_res = nested_getattr(eq_res.time_slice[0], field)
-      psi_org = eq_org.time_slice[0].profiles_1d.psi
-      psi_res = eq_res.time_slice[0].profiles_1d.psi
-      x_org = abs(psi_org - psi_org[0]) / abs(psi_org[-1] - psi_org[0])
-      x_res = abs(psi_res - psi_res[0]) / abs(psi_res[-1] - psi_res[0])
-      axes[idx].plot(x_org, val_org, label=f"t={t}", color=color)
-      axes[idx].scatter(x_res, val_res, color=color, marker='.')
-    axes[idx].legend()
     axes[idx].set_title(field[-1])
     axes[idx].set_ylabel(field[-1])
     axes[idx].set_xlabel('psi_norm')
+    for i_t, t in enumerate(T_LIST):
+      for num, key in enumerate(dbs.keys()):
+        eq = dbs[key].get_slice('equilibrium', time_requested=t, **GET_KWARGS)
+        val = nested_getattr(eq.time_slice[0], field)
+        psi = eq.time_slice[0].profiles_1d.psi
+        psi_norm = abs(psi - psi[0]) / abs(psi[-1] - psi[0])
+        if num == 0:
+          axes[idx].plot(psi_norm, val, label=f"t={t}", color=colors[i_t])
+        else:
+          axes[idx].scatter(psi_norm, val, color=colors[i_t], marker='.')
+    axes[idx].legend()
 
   fig.tight_layout(rect=[0, 0.03, 1, 0.95])
   fig.savefig(EQUILIBRIUM_FIGURE_PATH)
 
-  #################
-  # COIL CURRENTS #
-  #################
+def pf_active_plots(dbs):
   coil_dict = {}
-  pfa_org = db_org.get('pf_active')
-  pfa_res = db_res.get('pf_active')
+  pfas = {
+    key: db.get('pf_active')
+    for key, db in dbs.items()
+  }
   nrows, ncols = (4, 4)
   fig, axes = plt.subplots(nrows=nrows, ncols=nrows, figsize=(12,12))
   axes = axes.flatten()
-  for coil in pfa_org.coil:
-    coil_name = str(coil.name)
-    if coil_name not in coil_dict:
-      coil_dict[coil_name] = max(coil_dict.values(), default=-1) + 1
-    axes[coil_dict[coil_name]].set_title(coil_name)
-    axes[coil_dict[coil_name]].set_ylabel('current')
-    axes[coil_dict[coil_name]].set_xlabel('time')
-    axes[coil_dict[coil_name]].plot(pfa_org.time, coil.current.data, label='org', **plot_kwargs)
-  for coil in pfa_res.coil:
-    coil_name = str(coil.name)
-    axes[coil_dict[coil_name]].plot(pfa_res.time, coil.current.data, label='res', **plot_kwargs)
-    axes[coil_dict[coil_name]].legend()
+  for key, pfa in pfas.items():
+    for coil in pfa.coil:
+      coil_name = str(coil.name)
+      if coil_name not in coil_dict:
+        coil_dict[coil_name] = max(coil_dict.values(), default=-1) + 1
+        axes[coil_dict[coil_name]].set_title(coil_name)
+        axes[coil_dict[coil_name]].set_ylabel('current')
+        axes[coil_dict[coil_name]].set_xlabel('time')
+      axes[coil_dict[coil_name]].plot(pfa.time, coil.current.data, label=key, **PLOT_KWARGS)
+      axes[coil_dict[coil_name]].legend()
   fig.tight_layout(rect=[0, 0.03, 1, 0.95])
   fig.savefig(COIL_FIGURE_PATH)
-
-  db_org.close()
-  db_res.close()
 
 if __name__ == '__main__':
   main()
