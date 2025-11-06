42dc80ccc7c40886086d8db6e319aaaced455457
Mike Sanders <msanders@ignitioncomputing.com>
2025-11-06 15:19:01 +0100

    speedup nice

--- scenario_configs/105073_torax_nice/automate_runs.sh
+++ scenario_configs/105073_torax_nice/automate_runs.sh
@@ -1,11 +1,11 @@
 # This file expects to be run inside the parent directory
 
 SHOT_NR="105073"
-SOURCE_URI="imas:mdsplus?path=/home/ITER/dubrovm/public/imasdb/iter/3/105073/5"
+SOURCE_URI="imas:mdsplus?path=/home/ITER/dubrovm/public/imasdb/iter/3/105073/6"
 BACKUP_URI="imas:hdf5?path=/home/ITER/sanderm/public/imasdb/ITER/4/666666/3"
 SINK_URI="imas:hdf5?path=$(pwd)/tmp/data/${SHOT_NR}_in"
-N_TIMESLICES=11
-T_LIST=(10)
+N_TIMESLICES=41
+T_LIST=(25 100 175)
 
 basedir="$(dirname "$(dirname "$PWD")")"
 
