8ada959b9eb0d6398aba0e69b14f09856fe72f57
Mike Sanders <msanders@ignitioncomputing.com>
2026-02-24 11:13:01 +0100

    add extra preprocessing steps, TSTART in scenario_config, add back core_profiles in torax input, fix dir name to actual scenario

--- workflows/torax_nice_controller/create_runnable_files.sh
+++ workflows/torax_nice_controller/create_runnable_files.sh
@@ -14,5 +14,6 @@ for file in "${files[@]}"; do
   fi
   sed -i "s|\[BASEDIR_PLACEHOLDER\]|$PWD|g" "$SUBDIR/$file"
   sed -i "s|\[SUBDIR_PLACEHOLDER\]|$SUBDIR|g" "$SUBDIR/$file"
+  sed -i "s|\[TSTART_PLACEHOLDER\]|$TSTART|g" "$SUBDIR/$file"
   sed -i "s|\[SHOT_NR\]|$SHOT_NR|g" "$SUBDIR/$file"
 done
