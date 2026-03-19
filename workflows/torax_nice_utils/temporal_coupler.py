b57f808eb38d3eeb1f7ee694375ce85b46350210
Mike Sanders <msanders@ignitioncomputing.com>
2026-03-19 15:18:38 +0100

    add temporal coupler to torax nice controller workflow

---
+++ workflows/torax_nice_utils/temporal_coupler.py
@@ -0,0 +1,281 @@
+"""
+MUSCLE3 actor performing IDS timeslice accumulation.
+
+This actor can receive timeslices for various IDSs at the same time on the S
+port, keeping track of whether or not it was the last timeslice for a given
+inner loop. Optionally the 't_next' S port is used to override this behavior
+and match with the last timeslice of a specific actor. It then sends out all
+the IDSs with all accumulated timeslices on the O_F port.
+
+If no actors are available that pass information for the next_timestep, it
+will default to None and this actor will only be able to receive the first
+timeslice.
+
+This actor might have difficulty handling other actors with dynamic timesteps
+that cannot accurately predict whether their current timeslice will be the
+last, possibly leading to deadlocks. It is advised to use predictable or
+constant timestepping.
+"""
+
+import logging
+from typing import Dict, List
+
+from imas import DBEntry, IDSFactory
+from imas.ids_defs import IDS_TIME_MODE_INDEPENDENT
+from libmuscle import Instance, InstanceFlags, Message
+from ymmsl import Operator
+
+from imas_muscle3.utils import get_port_list
+
+logger = logging.getLogger()
+
+
+import logging
+from typing import Any, Optional, Tuple, Dict
+
+from libmuscle import Instance, InstanceFlags, Message
+from libmuscle.runner import run_simulation
+from ymmsl import (
+        Component, Conduit, Configuration, Model, Operator, Ports, Settings)
+
+
+class DataCache:
+    """Stores data from received messages and interpolates as needed.
+
+    This keeps the data and timestamps from the last two messages
+    received from a peer, and interpolates between them to produce data
+    for intermediate time points.
+    """
+    def __init__(self) -> None:
+        """Create a DataCache.
+
+        The cache starts out empty.
+        """
+        self.t_cur: Optional[float] = None
+        self.data_cur: Optional[Any] = None
+        self.t_next: Optional[float] = None
+        self.data_next: Optional[Any] = None
+
+    def add_data(self, t: float, data: Any) -> None:
+        """Add new data to the cache.
+
+        If the cache is currently empty, both the current and the next
+        data item are set to the new data, at which point we can
+        interpolate only for that exact point. As the next message
+        arrives, the new data item is saved as the 'next' value, and
+        the previous 'next' value becomes the 'cur' value.
+        """
+        if self.t_cur is None:
+            self.t_cur = t
+            self.data_cur = data
+
+            self.t_next = t
+            self.data_next = data
+        else:
+            self.t_cur = self.t_next
+            self.data_cur = self.data_next
+
+            self.t_next = t
+            self.data_next = data
+
+    def get_data(self, t: float) -> Tuple[float, Any]:
+        """Return a data value for a given time point.
+
+        This function interpolates the stored data to produce an
+        intermediate value. In this example, we simply assume that
+        each data item is valid until it is replaced by the next item.
+        Hence, no calculation is needed, we just return the current
+        value. A more advanced, model-specific coupler would interpolate
+        here.
+
+        This value is returned with its corresponding time point here,
+        because it makes it easier to see what happens in the log file.
+        If `t` is returned instead, as should be done when there's an
+        actual interpolation, then to the two submodels being connected
+        it looks exactly as if they are running in lockstep.
+
+        If one of the models runs for longer than the other model, then
+        the shorter-running model will stop producing new data while
+        the longer-running model still needs inputs. In this case, `t`
+        can be beyond `self.t_next`, and we need to extrapolate. Here
+        we do that by returning the `next` data rather than the `cur`
+        data. This also covers the corner case of both models hitting
+        the exact same timepoint.
+        """
+        if self.t_next <= t:
+            return self.t_next, self.data_next
+        else:
+            return self.t_cur, self.data_cur
+
+
+class Peer:
+    """Tracks state for a peer.
+
+    This is a helper class which for one of the peers keeps track of
+    the most recent messages it has sent, at which point in simulated
+    time the most recent message was received, when the next message
+    needs to be sent, and what the next timepoint of the model is.
+
+    This information can then be used to determine whether the model is
+    still running, whether we can receive from it or whether we need to
+    send a message to it first. For the latter, it also checks whether
+    the necessary data is available.
+
+    Finally, this class does the actual communication with the peer,
+    via the instance object.
+    """
+    def __init__(
+            self, instance: Instance, in_port: str, out_port: str,
+            resume_from_state: Any = None) -> None:
+        """Create a Peer object.
+
+        This also receives an initial message from the peer model, and
+        uses the received data to initialise the cache and the state.
+
+        Args:
+            instance: The instance to use for communication
+            in_port: The port to receive on for this peer
+            out_port: The port to send on for this peer
+        """
+        self.instance = instance
+        self.in_port = in_port
+        self.out_port = out_port
+        self.cache = DataCache()
+
+        if resume_from_state:
+            self.cache.t_cur = resume_from_state['cache.t_cur']
+            self.cache.data_cur = resume_from_state['cache.data_cur']
+            self.cache.t_next = resume_from_state['cache.t_next']
+            self.cache.data_next = resume_from_state['cache.data_next']
+            self.rcvd = resume_from_state['rcvd']
+            self.to_send = resume_from_state['to_send']
+            self.next = resume_from_state['next']
+        else:
+            msg = self.instance.receive(self.in_port)
+            self.cache.add_data(msg.timestamp, msg.data)
+            self.rcvd = msg.timestamp
+            self.to_send = msg.timestamp
+            self.next = msg.next_timestamp
+
+    def done(self) -> bool:
+        """Return whether we are done commmunicating with this peer."""
+        return self.to_send is None
+
+    def can_receive(self) -> bool:
+        """Return whether we can receive a message from this peer."""
+        return self.next is not None and self.next <= self.to_send
+
+    def receive(self) -> None:
+        """Receive a message from this peer and update the cache."""
+        msg = self.instance.receive(self.in_port)
+        self.cache.add_data(msg.timestamp, msg.data)
+        self.rcvd = msg.timestamp
+        self.next = msg.next_timestamp
+
+    def can_send(self, peer_rcvd: float, peer_next: Optional[float]) -> bool:
+        """Return whether we can send to this peer.
+
+        This determines whether our next interaction with the peer
+        should be sending a message to it, which depends on whether
+        the peer will try to receive again, and on whether we have
+        data available to send to it.
+
+        The latter depends on whether we have received the data from
+        the other peer that we need to create a message for the time
+        point at which this peer expects to receive. That information
+        is passed into this function.
+
+        Args:
+            peer_recvd: When we last received from the other peer.
+            peer_next: If and when the other peer will send again.
+        """
+        if self.to_send is None:
+            return False
+        return self.to_send <= peer_rcvd or peer_next is None
+
+    def send(self, t: float, data: Any) -> None:
+        """Send the next message to this peer.
+
+        This sends a message to the peer containing the given data
+        and timestamp, and marks this send as done.
+
+        Args:
+            t: Timestamp corresponding to the data
+            data: Data to send
+        """
+        self.instance.send(self.out_port, Message(t, self.next, data))
+        self.to_send = self.next
+
+    def get_state(self) -> Dict[str, Any]:
+        """Return the current state of this object as a MUSCLE-serializable dict
+        """
+        return {'cache.t_cur': self.cache.t_cur,
+                'cache.data_cur': self.cache.data_cur,
+                'cache.t_next': self.cache.t_next,
+                'cache.data_next': self.cache.data_next,
+                'rcvd': self.rcvd,
+                'to_send': self.to_send,
+                'next': self.next}
+
+
+def main() -> None:
+    """Model component connecting two scale-overlapping submodels.
+
+    This component sits in between two scale-overlapping submodels
+    running at different (and potentially variable) timesteps and
+    ensures that each of these peers receives a message whenever it
+    expects one, and can send a message whenever it expects to do so.
+
+    This function extends :func:`temporal_coupler` with checkpointing
+    capabilities.
+    """
+    instance = Instance({
+        Operator.O_I: ['a_out', 'b_out'],
+        Operator.S: ['a_in', 'b_in']},
+        InstanceFlags.USES_CHECKPOINT_API | InstanceFlags.SKIP_MMSF_SEQUENCE_CHECKS)
+
+    while instance.reuse_instance():
+        # if instance.resuming():
+        #     state = instance.load_snapshot().data
+        #     if state is not None:
+        #         a = Peer(instance, 'a_in', 'a_out', state['a'])
+        #         b = Peer(instance, 'b_in', 'b_out', state['b'])
+        # if instance.should_init():
+        #     # Receive initial messages and initialise state
+        #     a = Peer(instance, 'a_in', 'a_out')
+        #     b = Peer(instance, 'b_in', 'b_out')
+        a = Peer(instance, 'a_in', 'a_out')
+        b = Peer(instance, 'b_in', 'b_out')
+
+        # Send and receive as needed
+        while not a.done() or not b.done():
+            if a.can_receive():
+                print('receive a', a.rcvd, a.to_send, a.next, b.rcvd, b.to_send, b.next)
+                a.receive()
+            elif b.can_receive():
+                print('receive b', a.rcvd, a.to_send, a.next, b.rcvd, b.to_send, b.next)
+                b.receive()
+            elif a.can_send(b.rcvd, b.next):
+                print('send a', a.rcvd, a.to_send, a.next, b.rcvd, b.to_send, b.next)
+                t, data = b.cache.get_data(a.to_send)
+                a.send(t, data)
+            elif b.can_send(a.rcvd, a.next):
+                print('send b', a.rcvd, a.to_send, a.next, b.rcvd, b.to_send, b.next)
+                t, data = a.cache.get_data(b.to_send)
+                b.send(t, data)
+
+            # t_cur = min(a.rcvd, b.rcvd)
+            # if instance.should_save_snapshot(t_cur):
+            #     instance.save_snapshot(Message(
+            #             t_cur, data={'a': a.get_state(), 'b': b.get_state()}))
+
+        # t_cur = min(a.rcvd, b.rcvd)
+        # if instance.should_save_final_snapshot():
+        #     instance.save_final_snapshot(Message(t_cur))
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
+    main()
